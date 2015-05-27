e3dc0b2d0a604207253015a703fb0b779b90ae95
Palaniappan, Sathish <sapalaniappan@paypal.com>
2015-05-26 23:50:55 -0700

    Add

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ urlpatterns = patterns(
     'api.views',
     url(r'^users/$', 'user_list', name='user_list'),
     url(r'^users/(?P<pk>[0-9]+)$', 'user_detail', name='user_detail'),
-    url(r'^users/(?P<wuser__id>[0-9]+)$', 'user_preferences', name='user_preferences'),
-    url(r'^users/(?P<pk>[0-9]+)$', 'user_photos_by_id', name='user_photos_by_id'),
-    url(r'^users/(?P<wuser__id>[0-9]+)$', 'user_photos_by_user', name='user_photos'),
-    url(r'^users/(?P<wuser__id>[0-9]+)$', 'user_relations', name='user_relations'),
+    url(r'^users/preferences/(?P<wuser__id>[0-9]+)$', 'user_preferences', name='user_preferences'),
+    url(r'^users/photos/(?P<pk>[0-9]+)$', 'user_photos_by_id', name='user_photos_by_id'),
+    url(r'^users/photos/user/(?P<wuser__id>[0-9]+)$', 'user_photos_by_user', name='user_photos'),
+    url(r'^users/relations/(?P<wuser__id>[0-9]+)$', 'user_relations', name='user_relations'),
 )
