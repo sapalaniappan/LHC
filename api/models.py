57e4e60865a276c4cc8334125278d9b18a6ffa4e
Palaniappan, Sathish <sapalaniappan@paypal.com>
2015-07-31 10:11:32 -0700

    Adding userView

--- api/models.py
+++ api/models.py
@@ -28,7 +28,7 @@ class Wuser(models.Model):
 
 class WuserPhoto(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    wuser_id = models.BigIntegerField()
+    #wuser_id = models.BigIntegerField()
     wuser = models.ForeignKey(Wuser, related_name='photos')
     photo_name = models.CharField(max_length=10, blank=True, null=True)
     time_created = models.DateTimeField(blank=True, null=True)
