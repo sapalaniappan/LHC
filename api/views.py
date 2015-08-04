8319dbe6186f091e6e2553aac4d3e77ca7da500a
Palaniappan, Sathish <sapalaniappan@paypal.com>
2015-08-04 02:46:55 -0700

    Adding detail serializer

--- api/views.py
+++ api/views.py
@@ -195,7 +195,7 @@ def user_photos(request, id):
         data['id']=row[0]
         wuser=Wuser.objects.get(id=id)
         serializer1 = WuserSerializer(data=wuser)
-        if serializer.is_valid():
+        if serializer1.is_valid():
             data['wuser']=serializer1.data
         serializer = WuserPhotoUpdateSerializer(data=data)
         if serializer.is_valid():
